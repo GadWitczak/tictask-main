2f455f52a0728be6048c9fc2d5283a7f58a11b33
GadWitczak <119819421+GadWitczak@users.noreply.github.com>
2025-01-29 18:25:41 -0300

    Update index.js

--- Task_management/client/src/index.js
+++ Task_management/client/src/index.js
@@ -1,8 +1,18 @@
-import react from 'react';
-import  ReactDOM  from 'react-dom';
-import App from './App'
+//import react from 'react';
+//import  ReactDOM  from 'react-dom';
+//import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
 import Loginform from './loginform'
 import createacc from './createacc'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 
-ReactDOM.render(<App />,document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+
+
+
