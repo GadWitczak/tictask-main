1179e0347f73ca67abb9bb841f0475565f77faa1
PEDRO <pedrooliveira2702@gmail.com>
2025-01-30 00:49:38 -0300

    f

--- Task_management/client/src/tasks.js
+++ Task_management/client/src/tasks.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import react from 'react';
 import React, { ReactDOM } from 'react';
 import Loginform from './loginform';
@@ -45,51 +45,5 @@ render(){
     )
 }
 }
-=======
-import react from 'react';
-import React, { ReactDOM } from 'react';
-import Loginform from './loginform';
-import Createacc from './createacc'
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import { useSearchParams } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-const Task1=()=>{
-   const search=useLocation().search;
-   const tasks=new URLSearchParams(search).get('task')
-    var text="";
-    for(var x in tasks){
-        
-        if(tasks[x]==":"){
-            text+=tasks[x]+"\n";
-        }
-        else{
-            text+=tasks[x];
-        }
-    }
-    
- 
-  
-    return(
-        <>
-       <p >{text}</p>
-     
-        </>
-    )
-}
-class Tasks extends React.Component{
-    constructor(){
-        super();
-        
-    }
-    
-render(){
-    return(
-        <>
-       <p> <Task1/></p>
-      
-       </>
-    )
-}
-}
->>>>>>> refs/remotes/origin/front_1
+
 export default Tasks;
